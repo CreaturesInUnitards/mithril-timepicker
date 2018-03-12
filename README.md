629b7ba01637f6c90396c3e11c3798401deb9d09
scotty <portrayme@mac.com>
2018-03-12 10:05:14 -0700

    add reset

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ Pick a time! But only if you're using Mithril, and only for flexbox-capable brow
 
 ### Demo
 
-[mithril-timepicker at flems.io](https://tinyurl.com/y877gfgz)
+[mithril-timepicker at flems.io](https://tinyurl.com/y74cyanu)
 
 ### Installation
 
@@ -19,21 +19,38 @@ You'll want to bring in either src/style.sass or src/style.css, depending on you
 ### Usage
 
 ```
-const TimePicker = require('path/to/mithril-timepicker.js')
+const TimePicker = require('mithril-timepicker')
 
-m(TimePicker, { tfh: true, increment: 15, onchange: myTimeHandler })
+// make a reference to an object with {h, m}
+const myTime = { 
+    h: mySpecialDate.getHours(), 
+    m: mySpecialDate.getMinutes() 
+}
+
+m(TimePicker, {
+    time: myTime, // time passed in MUST be a reference, not a literal
+    onchange: ({h, m}) => {
+        mySpecialDate.setHours(h, m, 0, 0)
+    })     
+})
+
+// if you want to wipe the slate clean
+TimePicker.reset(myTime)
+
 ```
 
 ### API
 
-Optional `attrs` to pass to the component instance:
+`attrs` to pass to the component instance:
 
-| ATTRIBUTE            | TYPE     | DESCRIPTION              |
-| :------------------- | :------  | :----------------------- |
-| `time`           | Object   | {h, m} e.g. `{ h: 23, m: 59 }`. Hour is always 0-23 |
-| `tfh`            | Boolean  | 24-hour time, defaults to false |
-| `increment`      | Int  | 5- or 15- minute options per hour, defaults to 5 |
-| `onchange`            | Function  | Handler. Receives { h, m } as its argument|
+| ATTRIBUTE    | TYPE             | Required? | DESCRIPTION              |
+| :--          | :--              | :--       | :--                      |
+| `time`       | Object reference | YES       | {h, m} e.g. `{ h: 23, m: 59 }`. Hour is always 0-23 |
+| `tfh`        | Boolean          | no        | 24-hour time, defaults to false |
+| `increment`  | Int              | no        | 5- or 15- minute options per hour, defaults to 5 |
+| `onchange`   | Function         | no        | Handler. Receives { h, m } as its argument|
+
+
 
 ### Theming
 
--- mithril-timepicker.js
+++ mithril-timepicker.js
@@ -1,156 +1,202 @@
-;(function() {
+;(function(){
 	const m = (typeof global !== 'undefined')
 		? (global.m || require('mithril'))
 		: window.m
 
-
-	const a0 = n => `0${n}`
+	// leading zeroes
+	const a0 = n => n < 10 ? `0${n}` : n
 
 	const displayHours = (hours, tfh) =>
-		tfh && hours < 10
+		tfh
 			? a0(hours)
-			: tfh
-			? hours
 			: hours > 12
-				? hours - 12
-				: hours < 10 && hours > 0
-					? a0(hours)
-					: hours === 0
-						? 12
-						: hours
+			? a0(hours - 12)
+			: hours === 0
+				? 12
+				: a0(hours)
 
 	const displayMinutes = minutes => minutes < 10 ? a0(minutes) : minutes
 
-	const meridian = (hours, tfh) => tfh ? '' : hours > 11 ? 'PM' : 'AM'
+	const meridian = (hours) => hours < 12 ? 'AM' : 'PM'
 
-	const NumSpan = {
-		view({attrs, children}){
-			return m('span.num',
+	const numbers = (w, tfh, increment) => {
+		let arr = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
+		if (tfh && w == 'h') arr = arr.concat(arr.map(n => n + 12))
+		return w == 'h'
+			? arr
+			: w == 'r'
+				? ['AM', 'PM']
+				: increment == 5
+					? arr.map(n => n * 5)
+					: [0, 15, 30, 45]
+	}
+
+	const wrapperClass = (editing, tfh, increment) => {
+		if (!editing) return ''
+		const classList = []
+		classList.push(editing)
+		if (tfh) classList.push('tfh')
+		if (increment) classList.push(`i${increment}`)
+		return classList.join(' ')
+	}
+
+	const boxClass = (w, tfh, timeVal, n) =>
+		(w == 'r' && ((timeVal > 11 && n == 'PM') || (timeVal < 12 && n == 'AM'))) || 
+		(timeVal === n && (w == 'm' || ( tfh && w == 'h' ) || (!tfh && timeVal < 12)) || (!tfh && timeVal > 11 && n === timeVal - 12))
+			? 'current'
+			: ''
+
+	const Display = {
+		view: ({attrs}) =>
+			m('.display',
+				{ onclick: e => { attrs.show(e.target.id) } },
+				m('.display-num#h',
+					{ class: attrs.editing == 'h' ? 'current' : '' },
+					displayHours(attrs.time.h, attrs.tfh)
+				),
+				':',
+				m('.display-num#m',
+					{ class: attrs.editing == 'm' ? 'current' : '' },
+					displayMinutes(attrs.time.m)
+				),
+				!attrs.tfh && m('.display-num#r',
+				{ class: attrs.editing == 'r' ? 'current' : '' },
+				meridian(attrs.time.h, attrs.tfh)
+				)
+			)
+	}
+
+	const Box = {
+		view: ({attrs, children}) =>
+			m('.box',
 				{
-					id: attrs.id,
-					class: attrs.editing == attrs.id ? 'current' : '',
-					onclick: attrs.editing ? attrs.onclick : null
+					class: boxClass(attrs.type, attrs.tfh, attrs.time, attrs.n),
+					onclick: attrs.boxClick
 				},
 				children
 			)
-		}
 	}
 
-	function numbers(w, increment) {
-		const array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
-		return w == 'h'
-			? array
-			: increment == 5
-				? array.map(n => n * 5)
-				: [0, 15, 30, 45]
+	const hourClick = (time, tfh, n) => (e) => {
+		e.next = 'm'
+		time.h = tfh
+			? n
+			: time.h < 12
+				? n
+				: n + 12
 	}
 
-	function boxClass(time, thisValue, editing) {
-		let currentValue = typeof thisValue == 'string' 
-			? time.h < 12 ? 'AM' : 'PM'
-			: time[editing]
-		return thisValue === currentValue ? 'current' : ''
+	const minuteClick = (time, n) => (e) => {
+		e.next = 'r'
+		time.m = n
 	}
 
-	function editorClass(editing, increment){
-		const classes = ['editor']
-		if (editing == 'm' && increment == 15) classes.push('i15')
-		if (editing == 'r') classes.push('meridian')
-		return classes.join(' ')
+	const meridianClick = (time, n) => (e) => {
+		e.next = null
+		if (time.h < 12 && n == 'PM') time.h += 12
+		if (time.h > 11 && n == 'AM') time.h -= 12
 	}
 
-	const TimePicker = ({attrs}) => {
-		let time
-		let tfh
-		let editing
-		let increment
-		let begun = false
-
-		function toggleEditor(e) {
-			if (!begun) {
-				time = { h: 0, m: 0}
-				begun = true
-			}
-			let id = e.target.id || undefined
-			if (!(editing || id)) id = 'h'
-			chooseEditor(id)
-		}
-
-		function chooseEditor(id) {
-			editing = id
-			if (!editing) {
-				typeof attrs.onchange == 'function'
-					? attrs.onchange(time)
-					: console.log("This timepicker wasn't provided an onchange callback.")
-			}
-		}
-
-		function numberClick(e) {
-			e.stopPropagation()
-			chooseEditor(e.target.id)
-		}
-
-		function boxClick(n, e) {
-			e.stopPropagation()
-			if (typeof n == 'string') {
-				let delta = 0
-				if (time.h > 11 && n == 'AM') delta = -1
-				else if (time.h < 12 && n == 'PM') delta = 1
-				time.h += delta * 12
-			}
-			else time[editing] = n
-			chooseEditor(editing == 'h' ? 'm' : editing == 'm' && !tfh ? 'r' : undefined)
-		}
-
-		function box(n){
-			return m('.box'
-				, {
-					class: boxClass(time, n, editing),
-					onclick: boxClick.bind(null, n)
-				}
-				, editing == 'h' ? displayHours(n, tfh) : displayMinutes(n)
-			)
-		}
-
-
-
-		return {
-			oninit: ({attrs}) => {
-				time = attrs.time || time
-				tfh = attrs.tfh || tfh
-				increment = (attrs.increment === 5 || attrs.increment === 15) ? attrs.increment : 5
-				if (time) begun = true
-			},
-			view: (vnode) => m('.ciu-mithril-timepicker',
-				{ class: editing ? 'editing' : '' },
-				editing && m('.overlay', { onclick: toggleEditor }),
-				m('.display',
-					{
-						class: tfh ? 'tfh' : '',
-						onclick: toggleEditor
-					},
-					begun
-						? [
-							m(NumSpan, { id: 'h', editing: editing, onclick: numberClick }, displayHours(time.h, tfh)),
-							':',
-							m(NumSpan, { id: 'm', editing: editing, onclick: numberClick }, displayMinutes(time.m, tfh)),
-							!tfh && m('.num.meridian#r', { class: editing === 'r' ? 'current' : '' }, meridian(time.h, tfh))
-						]
-						: m('.clock')
-				),
-				editing && m('',
-				{ class: editorClass(editing, increment) },
-				editing == 'h'
-					? [
-						numbers(editing).map(n => box(n)),
-						tfh && numbers(editing).map(n => box(n + 12))
-					]
-					: editing == 'm'
-					? numbers(editing, increment).map(n => box(n))
-					: [ box('AM'), box('PM') ]
+	const Editor = {
+		view: ({attrs}) =>
+			m('.wrapper',
+				{ class: wrapperClass(attrs.editing, attrs.tfh, attrs.increment) },
+				m('.sled',
+					m('.editor', numbers('h', attrs.tfh).map(n => m(Box,
+						{
+							type: 'h',
+							tfh: attrs.tfh,
+							time: attrs.time.h,
+							n: n,
+							boxClick: hourClick(attrs.time, attrs.tfh, n)
+						},
+						displayHours(n, attrs.tfh)
+					))),
+					m('.editor', numbers('m', attrs.tfh, attrs.increment).map(n => m(Box,
+						{
+							type: 'm',
+							tfh: attrs.tfh,
+							time: attrs.time.m,
+							n: n,
+							boxClick: minuteClick(attrs.time, n)
+						},
+						displayMinutes(n)
+					))),
+					!attrs.tfh && m('.editor', numbers('r').map(n => m(Box,
+					{ type: 'r', time: attrs.time.h, n: n, boxClick: meridianClick(attrs.time, n) },
+					displayMinutes(n)
+					))),
 				)
 			)
+	}
+
+
+	const TimePicker = vnode => {
+		let editing
+
+		// Is Non-Negative Integer
+		const nn = n => n === (n >>> 0)
+
+		// valid time is e.g. { h: 23, m: 59 }
+		const isValid = t => (t && nn(t.h) && nn(t.m) && t.h < 24 && t.m < 60)
+
+		// set editor view
+		const setEditor = w => {
+			if (editing == w) closeEditor(vnode.attrs.onchange)
+			else editing = w
 		}
+
+		const closeEditor = (onchange, time) => {
+			editing = null
+			if (onchange) onchange(time)
+		}
+
+		// e.g. choosing an hour automatically advances to the minute chooser
+		const advanceViews = tfh => e => {
+			const next = e.next
+			if (next == 'm' || (!tfh && e.next == 'r')) setEditor(next)
+			if ((tfh && next == 'r') || next === null) closeEditor(vnode.attrs.onchange, vnode.attrs.time)
+		}
+
+		// if attrs.time is the wrong shape
+		const nothingToSomething = (time) => {
+			Object.assign(time, { h: 0, m: 0 })
+			setTimeout(() => { editing = 'h'; m.redraw() }, 100)
+		}
+
+		return {
+			view: ({attrs}) => {
+				const increment = ( attrs.increment == 5 || attrs.increment == 15 ) ? attrs.increment : 5
+				return m('.ciu-mithril-timepicker',
+					{
+						class: editing ? 'editing' : '',
+						onclick: advanceViews(attrs.tfh)
+					},
+					[
+						editing && m('.overlay', { onclick: closeEditor.bind(null, attrs.onchange, attrs.time) }),
+						isValid(attrs.time)
+							? m(Display, {
+								time: attrs.time,
+								tfh: attrs.tfh,
+								show: setEditor,
+								editing: editing
+							})
+							: m('.display', { onclick: nothingToSomething.bind(null, attrs.time) }, m('.clock')),
+						m(Editor, {
+							time: attrs.time,
+							editing: editing,
+							tfh: attrs.tfh,
+							increment: increment
+						})
+					]
+				)
+			}
+		}
+	}
+	
+	TimePicker.reset = time => () => {
+		delete time.h
+		delete time.m
 	}
 
 	try { module.exports = TimePicker}
